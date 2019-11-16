34f1dea583fcedb9b86db0f634e377c422f113f1
Melek Nur <melek05nur@gmail.com>
2019-11-16 18:08:10 +0100

    homework 1completed

---
+++ week1/kimlik.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+printf("Name:Esra Lara\n");
+printf("DOB:26 October 2004\n");
+printf("Mobile:+9045798747\n");
+}
---
+++ week1/merhabadunya.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    printf("Hello, world!\n");
+    printf("Hallo, wereld\n");
+    printf("Olá Mundo!\n");
+    printf("Merhaba,Dunya!\n");
+    printf("Sveika,pasaule!\n");
+    printf("Hai,dunia!\n");
+    printf("Saluton,Mundo!\n");
+    printf("Salamu,Dunia!\n");
+    printf("Ciao,Mundo!\n");
+}
