16a2584b49d152c245195bf103f53dc5201f5fbf
Melek Nur <melek05nur@gmail.com>
2019-11-30 19:51:30 +0100

    array1 completed

--- week3/array1.c
+++ week3/array1.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int ages []={12, 24, 36, 48, 60};
-    char isimBasHarfleri []={'M','B','H','M'};
-    float takipciSayim []={1.9, 2.0, 2.1};
-    printf("%d\n%c\n%2lf\n",ages[4],isimBasHarfleri[1],takipciSayim[0]);
+    int ages[] = {12, 24, 36, 48, 60};
+    char isimBasHarfleri[] = {'M','B','H','M','Z'};
+    float takipciSayim[] = {1.9, 2.0, 2.1, 2.2, 2.3};
+    int i = 0;
+    while(i < 5)
+    {
+        printf("%d-%c-%.1lf\n", ages[i], isimBasHarfleri[i], takipciSayim[i]);
+        i++;
+    }
+    return 0;
 }
