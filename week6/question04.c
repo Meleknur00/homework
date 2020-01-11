64e96aaab10b6628e79f54f91cae5a92fd846c20
Melek Nur <melek05nur@gmail.com>
2020-01-12 00:43:35 +0100

    week6 completed

---
+++ week6/question04.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n;
+
+    printf("Print odd numbers till: ");
+    scanf("%d", &n);
+
+    printf("All odd numbers from 1 to %d are: \n", n);
+
+   for(i=1; i<=n; i++)
+    {
+        if(i%2==1)
+        {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
